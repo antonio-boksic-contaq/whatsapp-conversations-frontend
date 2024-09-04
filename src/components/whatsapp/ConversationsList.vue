aee4f3aadb017eed711f9f2a954472dfce3353bb
Antonio Boksic <antonio.boksic@contaq.it>
2024-09-04 15:02:31 +0200

    Apportato modifiche richieste durante ferie agosto 2024: aggiunto notifiche, riordinato conversazioni in oridne cronologico, aggiunto bottone per incollare link brochure e altre piccole cose.

--- src/components/whatsapp/ConversationsList.vue
+++ src/components/whatsapp/ConversationsList.vue
@@ -5,7 +5,7 @@
       <input
         type="text"
         v-model="filter"
-        placeholder="Cerca per numero di telefono o idLista..."
+        placeholder="Cerca conversazione..."
         class="p-2 border rounded w-full" />
     </div>
     <!-- Lista delle conversazioni -->
@@ -22,13 +22,28 @@
           'bg-red-200': isOlderThan24Hours(conversation),
         }">
         <!-- friendly name e idLista -->
-        <div class="mb-2 text-center text-xl">
-          {{
-            conversation.friendlyName +
-            " (idLista: " +
-            conversation.idLista +
-            " )"
-          }}
+        <div class="mb-2 text-xl">
+          <!-- Nome sopra tutto -->
+          <div class="text-center">
+            {{ conversation.nominativo }}
+            <span
+              v-if="hasUnreadMessages(conversation)"
+              class="text-red-500 pl-2">
+              ●
+            </span>
+          </div>
+
+          <!-- FriendlyName e idLista sotto, con la notifica sulla destra -->
+          <div class="text-center">
+            <span>
+              {{
+                conversation.friendlyName +
+                " ( idLista: " +
+                conversation.idLista +
+                " )"
+              }}
+            </span>
+          </div>
         </div>
         <!-- info ultimo messaggio -->
         <div v-if="conversation.messages.length" class="truncate">
@@ -64,30 +79,34 @@
 </template>
 
 <script>
-import { ref, computed, onBeforeMount } from "vue";
+import { ref, computed, onBeforeMount, watch } from "vue";
 import { isOlderThan24Hours } from "@/utils/conversationChecks.js";
 import { formatDateForMsg } from "@/utils/date.js";
 
 export default {
   name: "ConversationsList",
-  props: ["conversations", "selectedConversation"],
-  emits: ["conversationSelected"],
+  props: ["conversations", "selectedConversation", "identity"],
+  emits: ["conversationSelected", "getConvs"],
   setup(props, context) {
     const filter = ref("");
     const selectedConversation = ref({});
 
     onBeforeMount(async () => {
-      // console.log("props.conversations", props.conversations);
+      console.log("props.conversations", props.conversations);
+      console.log(
+        "SELECTED CONVERSATION ON MOUNTED",
+        selectedConversation.value
+      );
     });
 
     const selectConversation = (conversation) => {
       selectedConversation.value = conversation;
-      // console.log(
-      //   "selecconv.value",
-      //   selectedConversation.value,
-      //   "conversation",
-      //   conversation
-      // );
+      console.log(
+        "selecconv.value",
+        selectedConversation.value,
+        "conversation",
+        conversation
+      );
       context.emit("conversationSelected", conversation);
     };
 
@@ -98,11 +117,69 @@ export default {
       return props.conversations.filter(
         (conversation) =>
           conversation.friendlyName
-            .toLowerCase()
+            ?.toLowerCase()
             .includes(filter.value.toLowerCase()) ||
-          conversation.idLista.toString().includes(filter.value.toLowerCase())
+          conversation.idLista
+            ?.toString()
+            .includes(filter.value.toLowerCase()) ||
+          conversation.nominativo
+            ?.toLowerCase()
+            .includes(filter.value.toLowerCase()) // Aggiunto il controllo per nominativo
       );
     });
+
+    // questa viene chiamata da ogni conversazione e gestisce il pallino della notifica
+    const hasUnreadMessages = (conversation) => {
+      const lastMessageIndex =
+        conversation.messages[conversation.messages.length - 1].state.index;
+      // console.log("conv friendly name", conversation.friendlyName);
+      // console.log("conv lastMessageIndex", lastMessageIndex);
+      // console.log("identity loggata", props.identity);
+
+      const loggedInParticipant = conversation.participants.find(
+        (participant) =>
+          participant.state.identity === props.identity.toString()
+      );
+
+      // console.log("LOGGEDINPAARTICIPANT", loggedInParticipant);
+
+      const lastReadMessageIndex =
+        loggedInParticipant.state.lastReadMessageIndex;
+
+      // questo mi serve quando ho solo inviato un messaggio ( dato che l'ho inviato io non viene segnato come letto da Twilio e quindi lastReadMessageIndex in questo caso sarebbe null)
+      if (lastReadMessageIndex === null) {
+        return false;
+      }
+
+      return lastMessageIndex !== lastReadMessageIndex;
+    };
+
+    // con questa watch se ricevo messaggio su conversazione aperta non appare il pallino della notifica
+    // perchè il conversation.setAllMessagesRead() viene altrimenti chiamato solo quando seleziono/clicco una conversazione
+    watch(
+      () => props.selectedConversation?.messages.length,
+      async (newVal, oldVal) => {
+        if (newVal > oldVal) {
+          //segno tutti i messaggi letti nella selected conversation
+          await props.selectedConversation.setAllMessagesRead();
+
+          //AVREI VOLUTO FARLO COSì: SOSTITUIRE OGGETTO NELLA LISTA CHE VIENE RENDERIZZATA IN MODO DA RIAPPLICARE LA LOGICA DI HASUNREADMESSAGES SU RERENDER
+          // MA NELLA COPIA DELL'OGGETTO PERDO DELLE PROPRIETà COME ad esempio: friendlyName, SetAllMessagesRead
+          // const matchingIndex = filteredConversations.value.findIndex(
+          //   (conv) => conv.sid === props.selectedConversation.sid
+          // );
+
+          // filteredConversations.value[matchingIndex] = {
+          //   ...props.selectedConversation,
+          // };
+
+          //quindi ho deciso di ricaricare direttamente le conversazioni
+          context.emit("getConvs", true);
+
+          //quando le ricarica mi leva la notifica perchè cmq selectedConversation mantiene il collegamento con Twilio e quindi su Twilio risultano appunto letti, vanno solo ricaricati come faccio appunto sopra con l'emit
+        }
+      }
+    );
 
     return {
       filter,
@@ -110,6 +187,7 @@ export default {
       selectConversation,
       isOlderThan24Hours,
       formatDateForMsg,
+      hasUnreadMessages,
     };
   },
 };
--- src/views/home/WhatsappConversationsView.vue
+++ src/views/home/WhatsappConversationsView.vue
@@ -10,7 +10,7 @@
     <div class="col-start-5 text-right">
       <Button
         @click="changeIdentity"
-        class="p-2 bg-blue-500 hover:bg-blue-600 text-white rounded">
+        class="p-2 bg-blue-500 hover:bg-blue-600 text-white p-button-rounded">
         Cambia login utente
       </Button>
     </div>
@@ -23,8 +23,10 @@
       class="col-start-1 bg-gray-200 rounded p-2 overflow-y-auto h-full border border-gray-300">
       <conversations-list
         :conversations="conversations"
+        :identity="identity"
         :selectedConversation="selectedConversation"
-        @conversationSelected="selectConversation">
+        @conversationSelected="selectConversation"
+        @getConvs="getConversations">
       </conversations-list>
     </div>
     <!-- PARTE DESTRA -->
@@ -34,10 +36,14 @@
         <h2
           class="text-3xl text-center font-bold mb-4 border-b border-gray-300 p-4 text-black">
           {{
+            "numero: " +
             selectedConversation.friendlyName +
             " - idLista: " +
-            selectedConversation.idLista
+            selectedConversation.idLista +
+            " - nominativo: " +
+            selectedConversation.nominativo
           }}
+          <!-- lista di partecipanti alla conversazione -->
           <!-- <div
             v-for="participant in selectedConversation.participants"
             :key="participant.sid">
@@ -58,6 +64,18 @@
             <div>
               {{ message.body }}
             </div>
+            <!-- <div>
+              "lastReadMessageIndex"
+              {{ loggedInParticipant.state.lastReadMessageIndex }}
+            </div>
+            <div>
+              "lastmessageindex"
+              {{
+                selectedConversation.messages[
+                  selectedConversation.messages.length - 1
+                ].state.index
+              }}
+            </div> -->
             <div class="text-right mt-2">
               {{ formatDateForMsg(message.dateCreated) }}
             </div>
@@ -77,12 +95,19 @@
         <div
           v-if="!isOlderThan24Hours(selectedConversation)"
           class="border-t border-gray-300 bg-white p-4">
-          <input
-            type="text"
-            v-model="newMessage"
-            @keyup.enter="sendMessage(selectedConversation)"
-            placeholder="Scrivi un messaggio..."
-            class="w-full p-2 border border-gray-300 rounded" />
+          <div class="flex">
+            <input
+              type="text"
+              v-model="newMessage"
+              @keyup.enter="sendMessage(selectedConversation)"
+              placeholder="Scrivi un messaggio..."
+              class="flex-grow p-2 border border-gray-300 rounded" />
+            <button
+              @click="addLinkToNewMessage()"
+              class="ml-2 p-2 bg-blue-500 hover:bg-blue-600 text-white rounded">
+              Incolla link brochure
+            </button>
+          </div>
         </div>
         <div
           v-else
@@ -143,6 +168,7 @@ export default {
     };
 
     const connectToTwilio = async () => {
+      console.log("CONNECTING TO TWILIO, IDENTITY:", identity.value);
       client.value = new Client(token.value);
       // console.log("client", client.value);
 
@@ -160,84 +186,85 @@ export default {
         if (conversation) {
           const messages = await conversation.getMessages();
           conversation.messages = messages.items;
+
+          //riordina conversazioni in base a ultimo messaggio
+          sortConversations();
         }
       });
 
-      // QUA MI SERVE LISTENER PER QUANDO STATO DEI MESSAGGI CAMBIA, AD ESEMPIO "RICEVUTO"/"LETTO"
+      // QUESTO è IL LISTENER CHE SI ATTIVA QUANDO C'è UNA NUOVA CONVERSAZIONE
+      // dato che parte prima del getConversations, voglio disattivarlo prima che venga chiamato getConversations perchè è inutile
+      // a parte che non capisco perchè parte questo listener per ogni conversazione sul mounted...
+      client.value.on("conversationAdded", async (conversation) => {
+        if (conversations.value.length !== 0) {
+          console.log("New conversation added:", conversation);
+          // console.log(
+          //   "New conversation conversation.items:",
+          //   conversation.items
+          // );
+
+          await addDataToConvs(conversation, true);
+
+          sortConversations();
+        }
+      });
     };
 
+    const loggedInParticipant = ref({});
+
     const getConversations = async () => {
+      //ottengo conversazioni non ordinate e a cui mancano dati
       const convs = await client.value.getSubscribedConversations();
       if (!convs.items.length > 0) {
         console.log("provaprova");
         return;
       }
 
-      const friendlyNames = convs.items.map(
-        (conversation) => conversation.friendlyName
+      console.log("convs", convs);
+      //aggiungi idlista,nominativo,partecipanti,messaggi eccecc
+      await addDataToConvs(convs);
+
+      // ordina cronologicamente le conversazioni in base a ultimo messaggio
+      sortConversations();
+
+      //conversations.value = convsWithDetails;
+      console.log("CONVERSATIONS", conversations.value);
+      loggedInParticipant.value = conversations.value[0].participants.find(
+        (participant) =>
+          participant.state.identity === identity.value.toString()
       );
-
-      const idListe = await getidListaForFriendlyNames(friendlyNames);
-      //console.log("idliste", idListe);
-
-      const idListaLookup = idListe.reduce((acc, item) => {
-        acc[item.friendlyName] = item.idLista;
-        return acc;
-      }, {});
-
-      const convsWithDetails = await Promise.all(
-        convs.items.map(async (conversation) => {
-          // Ottieni i messaggi della conversazione
-          const messagesResponse = await conversation.getMessages();
-          // QUESTO CODICE COMMENTATO LO LASCIO MA NON CI SERVE
-          // STAVO PROVANDO AD AGGIUNGERE "STA SCRIVENDO"/"VISUALIZZATO" E COSE DI QUESTO TIPO
-          // const messagesWithReceipts = messagesResponse.items.map((message) => {
-          //   const aggregatedDeliveryReceipt = message.aggregatedDeliveryReceipt;
-          //   message.deliveryReceipts = {
-          //     delivered: aggregatedDeliveryReceipt?.delivered || 0,
-          //     failed: aggregatedDeliveryReceipt?.failed || 0,
-          //     read: aggregatedDeliveryReceipt?.read || 0,
-          //     sent: aggregatedDeliveryReceipt?.sent || 0,
-          //     undelivered: aggregatedDeliveryReceipt?.undelivered || 0,
-          //     total: aggregatedDeliveryReceipt?.total || 0,
-          //   };
-          //   return message;
-          // });
-          conversation.messages = messagesResponse.items;
-
-          // Ottieni i partecipanti della conversazione
-          const participantsResponse = await conversation.getParticipants();
-          conversation.participants = participantsResponse;
-
-          // Aggiungi l'idLista alla conversazione
-          conversation.idLista =
-            idListaLookup[conversation.friendlyName] || null;
-
-          return conversation;
-        })
-      );
-
-      conversations.value = convsWithDetails;
-      console.log("CONVERSATIONS", conversations);
     };
 
-    const getidListaForFriendlyNames = async (friendlyNamesArray) => {
+    const sortConversations = () => {
+      conversations.value.sort((a, b) => {
+        const lastMessageA = a.messages[a.messages.length - 1];
+        const lastMessageB = b.messages[b.messages.length - 1];
+
+        // Controlla se ci sono messaggi
+        if (!lastMessageA) return 1;
+        if (!lastMessageB) return -1;
+
+        return (
+          new Date(lastMessageB.dateCreated) -
+          new Date(lastMessageA.dateCreated)
+        );
+      });
+    };
+
+    const getInternalDataForConvs = async (friendlyNamesArray) => {
       const params = { friendlyNames: friendlyNamesArray.join(",") };
 
-      const url = process.env.VUE_APP_API_URL + "/get-id-listas-for-convs";
+      const url = process.env.VUE_APP_API_URL + "/get-data-for-convs";
 
       const response = await apiStore.fetch(url, params);
       console.log("response", response);
       return response;
     };
 
-    const selectConversation = (conversation) => {
+    const selectConversation = async (conversation) => {
+      console.log("lunghezza", conversation.messages.length);
+      await conversation.setAllMessagesRead();
       selectedConversation.value = conversation;
-      console.log(selectedConversation.value);
-      // console.log(
-      //   "NELL EMIT DEL PADRE Selected conversation:",
-      //   selectedConversation.value
-      // );
     };
 
     const sendMessage = async (conversation) => {
@@ -277,6 +304,82 @@ export default {
       router.push({ name: "whatsapp-login" });
     };
 
+    const addLinkToNewMessage = () => {
+      const link =
+        "https://azionecontrolafame.b-cdn.net/wp-assets/uploads/2024/06/Brochure-2025_-DEF.pdf ";
+
+      newMessage.value = `${newMessage.value} ${link}`;
+    };
+
+    const addDataToConvs = async (convs, justOneConv = false) => {
+      //se la conversazione è singola convs non avrà proprietà items, che nel caso di convs multiple contiene appunto gli oggetti delle conversazioni
+      ////Dato che uso map su conversationsTOHandle ho bisogno che questo sia sempre un array, se convs.items esiste non c'è problema, altrimenti avvolgiamo convs in un array
+      const conversationsToHandle = convs.items ? convs.items : [convs];
+
+      console.log("conversationsToHandle", conversationsToHandle);
+
+      const friendlyNames = conversationsToHandle.map(
+        (conversation) => conversation.friendlyName
+      );
+
+      console.log("FRIENDLY NAMES", friendlyNames);
+
+      //con numeri di telefono trovo idLista associati
+      const idListe = await getInternalDataForConvs(friendlyNames);
+      console.log("idListe", idListe);
+
+      const idListaLookup = idListe.reduce((acc, item) => {
+        acc[item.friendlyName] = item.idLista;
+        return acc;
+      }, {});
+
+      const NominativoLookup = idListe.reduce((acc, item) => {
+        acc[item.friendlyName] = item.nominativo;
+        return acc;
+      }, {});
+
+      console.log("nominativolookup", NominativoLookup);
+
+      const convsWithDetails = await Promise.all(
+        conversationsToHandle.map(async (conversation) => {
+          // prendo messaggi della conversazione
+          const messagesResponse = await conversation.getMessages();
+          // QUESTO CODICE COMMENTATO LO LASCIO MA NON CI SERVE
+          // STAVO PROVANDO AD AGGIUNGERE "STA SCRIVENDO"/"VISUALIZZATO" E COSE DI QUESTO TIPO
+          // const messagesWithReceipts = messagesResponse.items.map((message) => {
+          //   const aggregatedDeliveryReceipt = message.aggregatedDeliveryReceipt;
+          //   message.deliveryReceipts = {
+          //     delivered: aggregatedDeliveryReceipt?.delivered || 0,
+          //     failed: aggregatedDeliveryReceipt?.failed || 0,
+          //     read: aggregatedDeliveryReceipt?.read || 0,
+          //     sent: aggregatedDeliveryReceipt?.sent || 0,
+          //     undelivered: aggregatedDeliveryReceipt?.undelivered || 0,
+          //     total: aggregatedDeliveryReceipt?.total || 0,
+          //   };
+          //   return message;
+          // });
+          conversation.messages = messagesResponse.items;
+
+          // Ottieni i partecipanti della conversazione
+          const participantsResponse = await conversation.getParticipants();
+          conversation.participants = participantsResponse;
+
+          // Aggiungo idLista e nominativo preso da query alla conversazione
+          conversation.idLista =
+            idListaLookup[conversation.friendlyName] || null;
+          conversation.nominativo =
+            NominativoLookup[conversation.friendlyName] || null;
+
+          return conversation;
+        })
+      );
+      if (justOneConv) {
+        conversations.value.push(convsWithDetails[0]);
+      } else {
+        conversations.value = convsWithDetails;
+      }
+    };
+
     return {
       conversations,
       selectedConversation,
@@ -290,9 +393,12 @@ export default {
       client,
       identity,
       loadingStore,
-      getidListaForFriendlyNames,
+      getInternalDataForConvs,
       formatDateForMsg,
       changeIdentity,
+      addLinkToNewMessage,
+      getConversations,
+      loggedInParticipant,
     };
   },
 };
